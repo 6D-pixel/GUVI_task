350008853bb6fd332dc78ca861e97fac499561db
6D-pixel <dinesh7rd@gmail.com>
2024-02-24 22:15:51 +0530

    added component
    done conponents for sign in and signup

---
+++ client/src/component/Heading.jsx
@@ -0,0 +1,5 @@
+function Heading({ label }) {
+  return <div className="text-4xl font-bold text-center">{label}</div>;
+}
+
+export default Heading;
---
+++ client/src/component/SubHeading.jsx
@@ -0,0 +1,3 @@
+export default function SubHeading({ label }) {
+  return <div className="text-slate-500 text-base pt-1 pb-4">{label}</div>;
+}
